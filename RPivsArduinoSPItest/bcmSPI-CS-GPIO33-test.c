5bec0a59161f4d024f3d6588ee40a36dda37dc22
Mehari Kassahun <atomehari@gmail.com>
2017-05-18 17:07:20 +0900

    The first modification made to the GPIO33-HEIGHT

--- RPivsArduinoSPItest/bcmSPI-CS-GPIO33-test.c
+++ RPivsArduinoSPItest/bcmSPI-CS-GPIO33-test.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 // Chip select on GPIO pin  GPIO3 
-#define nibp_CS  RPI_V2_GPIO_P1_33
+#define HEIGHT_CS  RPI_V2_GPIO_P1_33
 
 int main(int argc, char **argv) {
 	if (!bcm2835_init()) {
@@ -18,23 +18,26 @@ int main(int argc, char **argv) {
    //   bcm2835_spi_setChipSelectPolarity(nibp_CS, LOW);      // the default
 
 	//set the nibp_CS to be an output
-	//bcm2835_gpio_fsel(nibp_CS, BCM2835_GPIO_FSEL_OUTP);
+	//bcm2835_gpio_fsel(HEIGHT_CS, BCM2835_GPIO_FSEL_OUTP);
 
-	//bcm2835_gpio_write(nibp_CS,HIGH);
+	//bcm2835_gpio_write(HEIGHT_CS,HIGH);
 
 	//uint8_t mosi[10] = { 0x60, 0x00 };
-	uint8_t mosi_nibp[] = {
-        0x48, 0x45, 0x4C, 0x4C, 0x4F,
-        0x20, 
-        0x57, 0x4F, 0x52, 0x4C, 0x44,
-        0x0A 
+	uint8_t mosi_requestReadHeight[] = { 0xAA
 	};
-	uint8_t moutsi[12] = { 0 };
-	uint8_t miso_nibp[12] = { 0 };
+	uint8_t mosi_readJunkHeight[1];
+	//uint8_t moutsi[12] = { 0 };
+	uint8_t miso_readHeightData[] = { 0 };
+	
+
+	// ///////// This is the first part of the request  to the Height Sensor/////////////
+	//										   //
+	////////////////////////////////////////////////////////////////////////////////////
+
 	
 	//custom chip select set to low here
 
-	bcm2835_gpio_write(nibp_CS,LOW);
+	bcm2835_gpio_write(HEIGHT_CS,LOW);
 
 	// The  CS GPIO will be asserted at least 3 core clock cycles 
 	// before the msb of the first byte of the transefer
@@ -44,14 +47,12 @@ int main(int argc, char **argv) {
 	//create some delay to help match the clock and the cs
 	//bcm2835_delay(10);
 
-	for (char ret = 0; ret < 12; ret++) {
+	//for (char ret = 0; ret < 12; ret++) {
 		//transfer here
-		miso_nibp[ret] = bcm2835_spi_transfer(mosi_nibp[ret]);
-	}
+		miso_readJunkHeight[0] = bcm2835_spi_transfer(mosi_requestHeight[0]);
+	//}
 
 
-	
-	
 	//set custom chip select to high after finishing transfer here
 
 	//the CS line will be de-asserted no earlier than 1 core clock cycle 
@@ -59,15 +60,48 @@ int main(int argc, char **argv) {
 
 	//bcm2835_delayMicroseconds(.244);
 
-	bcm2835_gpio_write(nibp_CS,HIGH);
+	bcm2835_gpio_write(HEIGHT_CS,HIGH);
+
+
+	/////////// This is the second part of the call to the  Sensor for real data////////
+	//										  //
+	////////////////////////////////////////////////////////////////////////////////////
+
+	
+	//custom chip select set to low here
+
+	bcm2835_gpio_write(HEIGHT_CS,LOW);
+
+	// The  CS GPIO will be asserted at least 3 core clock cycles 
+	// before the msb of the first byte of the transefer
+
+	//bcm2835_delayMicroseconds(.777);
+
+	//create some delay to help match the clock and the cs
+	//bcm2835_delay(10);
+
+	//for (char ret = 0; ret < 12; ret++) {
+		//transfer here
+		miso_readJunkHeight[0] = bcm2835_spi_transfer(mosi_requestHeight[0]);
+	//}
+
+
+	//set custom chip select to high after finishing transfer here
+
+	//the CS line will be de-asserted no earlier than 1 core clock cycle 
+	//after the trailing edge of teh final clock pulse
+
+	//bcm2835_delayMicroseconds(.244);
+
+	bcm2835_gpio_write(HEIGHT_CS,HIGH);
 
 	//printf("Analogue level from SPI: %04x\n", miso[1] + ((miso[0] & 3) << 8));
-	for (char pri = 0; pri < 12; pri++) {
+	//for (char pri = 0; pri < 12; pri++) {
 		//if (!(ret % 6))
 		//	puts("");
 		//printf("%.2X ", rx[ret]);
-		printf("Character = %c\n", miso_nibp[pri] );
-	}
+		printf("Character = %c\n", miso_readHeightData[0] );
+	//}
 
 
 
