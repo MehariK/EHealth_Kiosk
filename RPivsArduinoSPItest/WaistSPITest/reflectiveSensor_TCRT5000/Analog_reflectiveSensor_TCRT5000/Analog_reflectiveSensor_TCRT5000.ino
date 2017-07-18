414d91a0838cf5c9a434bcf4a4fe80b7ec9140c2
Mehari <atomehari@gmail.com>
2017-07-18 18:11:17 +0900

    The firt attemp on using infrared reflective sensor is commited

---
+++ RPivsArduinoSPItest/WaistSPITest/reflectiveSensor_TCRT5000/Analog_reflectiveSensor_TCRT5000/Analog_reflectiveSensor_TCRT5000.ino
@@ -0,0 +1,27 @@
+//long int count2=0;
+int count1=1;
+int flg=0;
+int tcrt;
+int digSignalRead = 13;
+void setup(){
+pinMode(digSignalRead, INPUT);
+digitalWrite(digSignalRead, HIGH);
+Serial.begin(9600);
+}
+void loop(){
+//tcrt = analogRead(A0);
+tcrt = digitalRead(digSignalRead);
+Serial.println("This is the digital reading");
+Serial.println(tcrt);
+if (tcrt==1 & flg==0){
+count1++;
+flg=1;
+}
+if (tcrt==0) flg=0;
+
+ 
+Serial.println(count1);
+
+
+ 
+}
---
+++ RPivsArduinoSPItest/WaistSPITest/reflectiveSensor_TCRT5000/reflectiveSensor_TCRT5000.ino
@@ -0,0 +1,21 @@
+int digitalInputPin = 4;
+int count = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+
+
+
+pinMode(digitalInputPin, INPUT);
+digitalWrite(digitalInputPin, HIGH);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if (digitalRead(digitalInputPin) == 1){
+  count++;
+}
+Serial.println(count);
+
+}
